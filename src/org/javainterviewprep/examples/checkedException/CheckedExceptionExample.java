2ecadf25c146935c4d09e6aa81b57337cbe93d22
hiteshbargujar <hitesh.bargujar@hobsons.com>
2016-01-26 21:00:33 -0500

    no message

--- src/org/javainterviewprep/examples/checkedException/CheckedExceptionExample.java
+++ src/org/javainterviewprep/examples/checkedException/CheckedExceptionExample.java
@@ -1,5 +1,11 @@
 package org.javainterviewprep.examples.checkedException;
 
+//Read a File 'checkedException.txt'.
+//Handle or follow any instructions provided by compiler.
+//Resolve the instructions or warning provided by compiler.
 public class CheckedExceptionExample {
 
+	public static void main(String[] args) {
+		
+	}
 }
